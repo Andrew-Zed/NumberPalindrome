14b2c9ad6c693113d33646c87c9b11752b2bcaba
Andrew-Zed <andrew.awadyke@gmail.com>
2022-02-08 00:01:14 +0100

    first commit: is Number Palindrome?

---
+++ src/com/andrew/Main.java
@@ -0,0 +1,10 @@
+package com.andrew;
+
+public class Main {
+
+    public static void main(String[] args) {
+        NumberPalindrome numberPalindrome = new NumberPalindrome();
+        numberPalindrome.isPalindrome(1221);
+        numberPalindrome.isPalindrome(12222212);
+    }
+}
