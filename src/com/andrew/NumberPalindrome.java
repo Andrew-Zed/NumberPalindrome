b56e77e79a19dae6a358de87dbc64fdfbb585101
Andrew-Zed <andrew.awadyke@gmail.com>
2022-02-08 00:17:48 +0100

    Second commit

--- src/com/andrew/NumberPalindrome.java
+++ src/com/andrew/NumberPalindrome.java
@@ -53,10 +53,7 @@ public class NumberPalindrome {
         int reverse = 0;
         while(number != 0) {
             int lastDigit = number % 10;
-            reverse = reverse * 10;
-
-            reverse = reverse + lastDigit;
-
+            reverse = (reverse * 10) + lastDigit;
             number = number / 10;
         }
         if (temp == reverse){
